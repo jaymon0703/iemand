e70d6c1a748ee79a2fd8ba0535f1902203f4affb
Jasen <jaymon0703@gmail.com>
2018-03-16 15:51:45 +0200

    Fix vignette commit

--- R/give_name.R
+++ R/give_name.R
@@ -1,3 +1,11 @@
+#' Title
+#'
+#' @param sex
+#'
+#' @return
+#' @export
+#'
+#' @examples
 give_name <- function(sex = "female"){
   possible_names <- sa_names$first_name[sa_names$sex == sex]
   sample(possible_names, size = 1)
